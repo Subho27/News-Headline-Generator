571badbba8e27a4d4b1b372239f1565978313ecc
Subho27 <42699151+Subho27@users.noreply.github.com>
2024-05-02 01:28:32 +0530

    Final Submission

--- NewsHeadlineGenerator/app/src/main/java/com/example/newsheadlinegenerator/GetHeadlinesAPI.java
+++ NewsHeadlineGenerator/app/src/main/java/com/example/newsheadlinegenerator/GetHeadlinesAPI.java
@@ -6,7 +6,10 @@ import retrofit2.http.POST;
 
 public interface GetHeadlinesAPI {
 
-    @POST("api/data/")
-    Call<String[]> getHeadlines(@Body String article);
+    @POST("api/gptdata/")
+    Call<String[]> getGPTHeadlines(@Body String article);
+
+    @POST("api/t5data/")
+    Call<String[]> getT5Headlines(@Body String article);
 
 }
--- NewsHeadlineGenerator/app/src/main/java/com/example/newsheadlinegenerator/ui/home/HomeFragment.java
+++ NewsHeadlineGenerator/app/src/main/java/com/example/newsheadlinegenerator/ui/home/HomeFragment.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +20,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.newsheadlinegenerator.ApiCaller;
 import com.example.newsheadlinegenerator.GetHeadlinesAPI;
+import com.example.newsheadlinegenerator.R;
 import com.example.newsheadlinegenerator.databinding.FragmentHomeBinding;
 
 import org.json.JSONException;
@@ -32,6 +38,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private CharSequence choice = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,11 +48,42 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        EditText article = (EditText) binding.newsArticle;
-        Button submit = (Button) binding.submitButton;
-        TextView result = (TextView) binding.headlineResult;
+        Spinner spinner = (Spinner) binding.improveChoices;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getContext(),
+                R.array.choice_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                choice = adapter.getItem(position);
+            }
 
-        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        EditText article = (EditText) binding.newsArticle;
+        Button submitGpt = (Button) binding.submitGpt;
+        Button submitT5 = (Button) binding.submitT5;
+        TextView result = (TextView) binding.headlineResult;
+        ProgressBar progress = (ProgressBar) binding.progress;
+        RelativeLayout improve = (RelativeLayout) binding.improveSection;
+        Button improveButton = (Button) binding.improveButton;
+
+        improveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Your choice has been recorded for improvement " + choice, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        submitGpt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String articleText = article.getText().toString();
@@ -54,9 +92,13 @@ public class HomeFragment extends Fragment {
                 }
                 else {
                     try {
-                        System.out.println("Call");
-                        String apiUrl = "https://ede4-34-171-144-64.ngrok-free.app";
-                        fetchHeadlines(apiUrl, articleText, result);
+                        if(improve.getVisibility() == View.VISIBLE) {
+                            improve.setVisibility(View.GONE);
+                        }
+                        result.setText("");
+                        progress.setVisibility(View.VISIBLE);
+                        String apiUrl = "https://0167-35-229-157-150.ngrok-free.app";
+                        fetchHeadlinesGpt(apiUrl, articleText, result, progress, improve);
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
@@ -64,8 +106,29 @@ public class HomeFragment extends Fragment {
             }
         });
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        submitT5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String articleText = article.getText().toString();
+                if(articleText.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter the article for what you want to generate Headline.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        if(improve.getVisibility() == View.VISIBLE) {
+                            improve.setVisibility(View.GONE);
+                        }
+                        result.setText("");
+                        progress.setVisibility(View.VISIBLE);
+                        String apiUrl = "https://0167-35-229-157-150.ngrok-free.app";
+                        fetchHeadlinesT5(apiUrl, articleText, result, progress, improve);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
         return root;
     }
 
@@ -75,7 +138,7 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
-    private void fetchHeadlines(String baseUrl, String searchQuery, TextView headlineTextView) {
+    private void fetchHeadlinesGpt(String baseUrl, String searchQuery, TextView headlineTextView, ProgressBar progressBar, RelativeLayout relativeLayout) {
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(1, TimeUnit.MINUTES)
@@ -90,27 +153,81 @@ public class HomeFragment extends Fragment {
                 .build();
 
         GetHeadlinesAPI api = retrofit.create(GetHeadlinesAPI.class);
-        Call<String[]> call = api.getHeadlines(searchQuery);
+        Call<String[]> call = api.getGPTHeadlines(searchQuery);
         call.enqueue(new Callback<String[]>() {
             @Override
             public void onResponse(Call<String[]> call, Response<String[]> response) {
                 if (response.isSuccessful()) {
+                    progressBar.setVisibility(View.INVISIBLE);
                     StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("Headlines generated by GPT : \n\n");
+                    int i = 1;
                     for (String headline : response.body()) {
-                        stringBuilder.append(headline).append("\n");
+                        stringBuilder.append(i).append(". ").append(headline).append("\n\n");
+                        i++;
                     }
                     headlineTextView.setText(stringBuilder.toString());
+                    relativeLayout.setVisibility(View.VISIBLE);
                 } else {
                     int code = response.code();
-                    String message = "Failed to fetch headlines (code: " + code + ")";
+                    String message = "Failed to fetch headlines GPT (code: " + code + ")";
                     Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
                 }
             }
 
             @Override
             public void onFailure(Call<String[]> call, Throwable t) {
-                String message = "Network error: " + t.getMessage();
+                String message = "Network error GPT : " + t.getMessage();
                 Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void fetchHeadlinesT5(String baseUrl, String searchQuery, TextView headlineTextView, ProgressBar progressBar, RelativeLayout relativeLayout) {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.MINUTES)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        GetHeadlinesAPI api = retrofit.create(GetHeadlinesAPI.class);
+        Call<String[]> call = api.getT5Headlines(searchQuery);
+        call.enqueue(new Callback<String[]>() {
+            @Override
+            public void onResponse(Call<String[]> call, Response<String[]> response) {
+                if (response.isSuccessful()) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("Headlines generated by T5 : \n\n");
+                    int i = 1;
+                    for (String headline : response.body()) {
+                        stringBuilder.append(i).append(". ").append(headline).append("\n\n");
+                        i++;
+                    }
+                    headlineTextView.setText(stringBuilder.toString());
+                    relativeLayout.setVisibility(View.VISIBLE);
+                } else {
+                    int code = response.code();
+                    String message = "Failed to fetch headlines T5 (code: " + code + ")";
+                    Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String[]> call, Throwable t) {
+                String message = "Network error T5 : " + t.getMessage();
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
     }
